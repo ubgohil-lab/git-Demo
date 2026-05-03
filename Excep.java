ff451ce01cb9d1869ddd2e967e7cd6521a704f6b
Urvashi <ubgohil13@gmail.com>
2026-05-03 22:05:24 +0530

    Add Excep

--- Excep.java
+++ Excep.java
@@ -1,3 +1,21 @@
 public class Excep {
-    
+    public static void main(String args[]){
+        int nums[] = { 1,2,3,4,5};
+
+        try{
+            for(int i = 0; i<nums.length+1; i++){
+                System.out.println("nums:" + nums[i]);
+            }
+            
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Error: "+ e);
+        }
+
+        finally {
+            System.out.println("Finally block");
+        }
+
+
+    }
 }
